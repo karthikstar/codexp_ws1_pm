461e7c08b6f85101570c8ab411d151512f1811ba
karthikstar <elkarthi2000@gmail.com>
2022-06-10 23:49:21 +0800

    edited home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import { FontAwesome } from "@expo/vector-icons";
 export default function HomeScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <Text>Homeee!</Text>
         <FontAwesome name="home" size = {50} color="red" />
       </View>
     ); 
