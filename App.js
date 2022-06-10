4da0c739b9d656c31ca9a93287ac35b24f19e904
karthikstar <64301417+karthikstar@users.noreply.github.com>
2022-06-10 18:40:41 +0800

    Updated welcome msg

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello worlddd!</Text>
+      <Text>goodbye worlddd!</Text>
       <StatusBar style="auto" />
     </View>
   );
